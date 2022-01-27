9966b347c3ce9552f4454495dd713886330dfeb6
Daniel Jesús Rodríguez serafin <90701015+Danrodsf@users.noreply.github.com>
2022-01-27 12:53:32 +0100

    Updated expire time in jwt

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,5 @@
 module.exports = {
   secret: process.env.AUTH_SECRET || "mrthcst12",
-  expires: process.env.AUTH_EXPIRES || "24h",
+  expires: process.env.AUTH_EXPIRES || "30d",
   rounds: process.env.AUTH_ROUNDS || 10,
 };
